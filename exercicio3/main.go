31c454c79837a7b58693e6bc351e4e61d6d7eaab
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-05-19 12:19:31 -0300

    exercicio 3

---
+++ exercicio3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+var nome string
+var sobrenome string
+var idade int
+sobrenome = "teste"
+var licenca_para_dirigir = true
+var estatura_da_pessoa int
+quantidadeDeFilhos := 2
+
+estatura_da_pessoa = 12
+
+fmt.Println(nome, sobrenome, idade, licenca_para_dirigir, estatura_da_pessoa, quantidadeDeFilhos)
+
+}
