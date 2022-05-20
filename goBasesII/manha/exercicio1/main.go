249aeadfa778f189ca3951011bacece8197cf6f5
Katielly Feitosa <katiellyfeitosa@outlook.com>
2022-05-20 11:26:30 -0300

    adding exercicio1 goBasesII

---
+++ goBasesII/manha/exercicio1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	funcionario1 := calcImposto(50000, 0.17)
+	funcionario2 := calcImposto(150000, 0.10)
+
+	fmt.Println("O funcioanrio 1 vai pagar", funcionario1, "de imposto.")
+	fmt.Println("O funcioanrio 2 vai pagar", funcionario2, "de imposto.")
+
+}
+
+func calcImposto(salario float32, taxaImposto float32) float32 {
+
+	return salario * taxaImposto
+}
